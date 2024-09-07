291cad5a30defe91f864648b9e2cd1b2bda62416
jesusherrera94 <jesusherrera1594@gmail.com>
2024-09-07 10:23:55 -0600

    read board from a file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include<vector>
+#include <fstream>
+#include <string>
 using std::cout;
 using std::vector;
+using std::ifstream;
+using std::string;
 
 void PrintBoard(vector<vector<int>> board) {
 	for(auto row : board) {
@@ -12,6 +16,18 @@ void PrintBoard(vector<vector<int>> board) {
     }
 }
 
+void ReadBoardFile(string path) {
+	ifstream board_file;
+  board_file.open(path);
+  if (board_file) {
+  	cout<<"The file has been opened successfully! \n";
+    string line;
+    while (getline(board_file, line)) {
+      cout<<line<<"\n";
+    }
+  }
+}
+
 int main() {
   // TODO: Declare a "board" variable here, and store
   // the data provided above.
@@ -20,6 +36,8 @@ int main() {
                             {0, 1, 0, 0, 0, 0},
                             {0, 1, 0, 0, 0, 0},
                             {0, 0, 0, 0, 1, 0}};
+  string file_path {"1.board"};
+  ReadBoardFile(file_path);
   PrintBoard(board);
   cout << "Hello!" << "\n";
 }
