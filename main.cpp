dffdbaf824d2b210f639630319b73e488fc7c3d5
jesusherrera94 <jesusherrera1594@gmail.com>
2024-09-06 21:38:55 -0600

    add print board functionallity

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 using std::cout;
 using std::vector;
 
+void PrintBoard(vector<vector<int>> board) {
+	for(auto row : board) {
+    	for(int col : row) {
+        	cout<<col;
+        }
+      	cout<<"\n";
+    }
+}
+
 int main() {
   // TODO: Declare a "board" variable here, and store
   // the data provided above.
@@ -11,6 +20,6 @@ int main() {
                             {0, 1, 0, 0, 0, 0},
                             {0, 1, 0, 0, 0, 0},
                             {0, 0, 0, 0, 1, 0}};
-  
+  PrintBoard(board);
   cout << "Hello!" << "\n";
 }
